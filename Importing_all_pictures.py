6bc1bc98cbb6653152b68db81019055d653443b3
VakantieModus <73619294+VakantieModus@users.noreply.github.com>
2023-03-22 16:20:51 +0100

    Update Importing_all_pictures.py

--- Importing_all_pictures.py
+++ Importing_all_pictures.py
@@ -11,23 +11,25 @@ import os
 """ Importing Data """
 path_to_all_data = r"C:\Users\caspe\OneDrive - TU Eindhoven\DS&AI 2022-2023\Q3\2AMV10 - Visual Analytics\2. Terrorists at CGCS\data\data"
 
-persons = next(os.walk(path_to_all_data))[1]
 
-paths = [x for x in os.walk(path_to_all_data)]
-#%%
-
-#importing the Image class from PIL package
-
-person_images = { }
 from PIL import Image
-for subdir, dirs, files in os.walk(path_to_all_data):
-    person = subdir[116:] #This is different for everyone as folders are named differently!!
-    person_images[person] = []
-    for file in files:
-        im_path = os.path.join(subdir, file)
-        str_im_path = str(im_path)
-        if str_im_path[-1] == 'g':
+#importing the Image class from PIL package
+def picture_import(path):
+    person_images = { }
+    
+    for subdir, dirs, files in os.walk(path_to_all_data):
+        person = subdir[116:] #This is different for everyone as folders are named differently!!
+        person_images[person] = []
+        for file in files:
+            im_path = os.path.join(subdir, file)
+            str_im_path = str(im_path)
+            if str_im_path[-1] == 'g':
+                
+                #read the image, creating an object
+                im = Image.open(im_path)
+                person_images[person].append(im)
             
-            #read the image, creating an object
-            im = Image.open(im_path)
-            person_images[person].append(im)
+    return person_images
+
+
+picture_dict = picture_import(path_to_all_data)
