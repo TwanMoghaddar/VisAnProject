db5812e2dc1606044e06ec85efa17945489bbf7f
TwanMoghaddar <a.a.moghaddar@student.tue.nl>
2023-04-04 14:07:21 +0200

    Updates

--- main.py
+++ main.py
@@ -12,46 +12,37 @@ Created on Fri Jan 14 20:44:17 2022
 @author: caspe
 """
 
-import dash                     #(version 1.0.0)
-from dash import dcc
-from dash import html, dash_table
-import dash_bootstrap_components as dbc
 import os
 
-
-
-import pandas as pd
-from dash import Input, Output, dcc, html
-from dash import Dash, Input, Output, callback, dash_table
-import pandas as pd
-import dash_bootstrap_components as dbc
-
 import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from dash import callback, dash_table
+from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-import os 
-from PIL import Image
-import numpy as np
-import plotly.express as px
 from dash.exceptions import PreventUpdate
-import matplotlib.pyplot as plt
-import mpld3
-#%%
+
+# %%
 
 """ Importing Data """
-path_to_all_data = r"C:\Users\caspe\OneDrive - TU Eindhoven\DS&AI 2022-2023\Q3\2AMV10 - Visual Analytics\2. Terrorists at CGCS\data\data"
-
+path_to_all_data = r"C:\Users\caspe\OneDrive - TU Eindhoven\DS&AI 2022-2023\Q3\2AMV10 - Visual Analytics\2. " \
+                   r"Terrorists at CGCS\data\data"
 
 from PIL import Image
-#importing the Image class from PIL package
+
+
+# importing the Image class from PIL package
 def picture_import(path):
     person_images = {}
     person_text = {}
     all_pictures = {}
     all_captions = {}
     for subdir, dirs, files in os.walk(path_to_all_data):
-        person = subdir[116:] #This is different for everyone as folders are named differently!!
+        person = subdir[116:]  # This is different for everyone as folders are named differently!!
         person_images[person] = []
         person_text[person] = []
 
@@ -60,60 +51,56 @@ def picture_import(path):
             im_path = os.path.join(subdir, file)
             str_im_path = str(im_path)
             if str_im_path[-1] == 'g':
-                
-                #read the image, creating an object
+                # read the image, creating an object
                 im = Image.open(im_path)
-                person_images[person].append((file,im))
-                
+                person_images[person].append((file, im))
+
                 all_pictures[file] = im
             if str_im_path[-1] == 't':
                 with open(str_im_path, 'r') as file:
                     data = file.read().replace('\n', '')
                     person_text[person].append((z, data))
                     all_captions[z] = data
-                    
+
     return person_images, person_text, all_pictures, all_captions
+
 
 data_import = picture_import(path_to_all_data)
 picture_dict = data_import[0]
 text_dict = data_import[1]
 all_pictures = data_import[2]
-all_captions =  data_import[3]
-#%%
+all_captions = data_import[3]
+# %%
 ######## Making Dataframe per person
 persons = picture_dict.keys()
 all_data_frames = {}
 for i in persons:
-    person_frame = pd.DataFrame(columns = ['Time' , 'Text', 'Picture'] )
+    person_frame = pd.DataFrame(columns=['Time', 'Text', 'Picture'])
     time = 0
     index = 0
     for pic_tup in picture_dict[i]:
         placed = False
         if text_dict[i]:
-            
+
             for text_tup in text_dict[i]:
                 if pic_tup[0][:-4] == text_tup[0][:-11]:
-                    person_frame.loc[index] = [time, text_tup[1],pic_tup[0]]
+                    person_frame.loc[index] = [time, text_tup[1], pic_tup[0]]
                     time += 1
                     index += 1
                     placed = True
                     break
         elif not placed:
-            person_frame.loc[index] = [time, 0,pic_tup[0]]
+            person_frame.loc[index] = [time, 0, pic_tup[0]]
             time += 1
             index += 1
         else:
-            person_frame.loc[index] = [time, 0,pic_tup[0]]
+            person_frame.loc[index] = [time, 0, pic_tup[0]]
             time += 1
             index += 1
-            
-    all_data_frames[i]=person_frame
-            
 
+    all_data_frames[i] = person_frame
 
-
-
-#%%
+# %%
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
@@ -139,7 +126,7 @@ sidebar = html.Div(
     [
         html.H2("Content", className="display-4"),
         html.Hr(),
- 
+
         dbc.Nav(
             [
                 dbc.NavLink("Homepage: Prediction", href="/page-1", active="exact"),
@@ -154,23 +141,20 @@ sidebar = html.Div(
     style=SIDEBAR_STYLE,
 )
 
-
 TEXT_STYLE = {
     'textAlign': 'center',
     'color': '#191970'
 }
 
-
-#Tittle + index reference
+# Tittle + index reference
 Content_header1 = dbc.Row(html.Div([
-    html.H1('Homepage: Prediction')], style = TEXT_STYLE))
+    html.H1('Homepage: Prediction')], style=TEXT_STYLE))
 Content_header2 = dbc.Row(html.Div([
-    html.H1('Page 2: Profile Overview')], style = TEXT_STYLE))
+    html.H1('Page 2: Profile Overview')], style=TEXT_STYLE))
 Content_header3 = dbc.Row(html.Div([
-    html.H1('Page 3: Text Analysis')], style = TEXT_STYLE))
+    html.H1('Page 3: Text Analysis')], style=TEXT_STYLE))
 Content_header4 = dbc.Row(html.Div([
-    html.H1('Page 4: Image Classifications')], style = TEXT_STYLE))
-
+    html.H1('Page 4: Image Classifications')], style=TEXT_STYLE))
 
 # Page Navigation
 index_page = html.Div([
@@ -181,13 +165,12 @@ index_page = html.Div([
     dcc.Link('Textual Analysis', href='/page-3'),
     html.Br(),
     dcc.Link('Image classification', href='/page-4')
-    
+
 ])
 
-                                                       
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content'),     
+    html.Div(id='page-content'),
 ])
 ################################## Page 1 ############################
 
@@ -197,25 +180,24 @@ page_1_layout = html.Div([
     html.Br(),
     sidebar,
     html.Br()
-                    
+
 ]
 )
 
 ##################################  Page 2    ################################## 
 dropdown1 = html.Div([
-    dcc.Dropdown( options=sorted(list(all_data_frames.keys())),id='demo-dropdown1'),
+    dcc.Dropdown(options=sorted(list(all_data_frames.keys())), id='demo-dropdown1'),
     html.Div(id='dd-output-container1')
 ])
-table1  = html.Div(id='table1')
+table1 = html.Div(id='table1')
 alert1 = dbc.Container(id='tbl_out1')
 
 dropdown2 = html.Div([
-    dcc.Dropdown( options=sorted(list(all_data_frames.keys())),id='demo-dropdown2'),
+    dcc.Dropdown(options=sorted(list(all_data_frames.keys())), id='demo-dropdown2'),
     html.Div(id='dd-output-container2')
 ])
-table2  = html.Div(id='table2')
+table2 = html.Div(id='table2')
 alert2 = dbc.Container(id='tbl_out2')
-
 
 page_2_layout = html.Div([
     html.Div(id='page-2-content'),
@@ -223,42 +205,35 @@ page_2_layout = html.Div([
     html.Br(),
     sidebar,
     dbc.Container([dbc.Row([
-            #Boxplot
-            dbc.Col([dropdown1,
-                     table1,
-                     alert1
+        # Boxplot
+        dbc.Col([dropdown1,
+                 table1,
+                 alert1
 
-                    ]), 
-            
-            dbc.Col([dropdown2,
-                     table2,
-                     alert2
+                 ]),
 
-                    ]),
-                ]),
-        
-        ]
-        
-        
-        )
-    
+        dbc.Col([dropdown2,
+                 table2,
+                 alert2
 
-                                
+                 ]),
+    ]),
+
+    ]
+
+    )
 
 ])
 
-
-
-
-##################################  Page 3   ################################## 
+##################################  Page 3   ##################################
 page_3_layout = html.Div([
     html.Div(id='page-3-content'),
     Content_header3,
     html.Br(),
     sidebar
 
-])            
-       
+])
+
 ##################################  Page 4   ################################## 
 
 page_4_layout = html.Div([
@@ -266,10 +241,10 @@ page_4_layout = html.Div([
     Content_header4,
     html.Br(),
     sidebar
-    
-])         
 
-                     
+])
+
+
 ################################### Callbacks Home-page ##################################
 @app.callback(dash.dependencies.Output("page-content", "children"), [dash.dependencies.Input("url", "pathname")])
 def render_page_content(pathname):
@@ -292,79 +267,81 @@ def render_page_content(pathname):
         ],
         className="p-3 bg-light rounded-3",
     )
-    
-                     
+
+
 ################################### Callbacks page 1 ##################################
 
 
 ################################### Callbacks page 2 ##################################
 
 ################################### User y ###################################
-@callback(Output('tbl_out1', 'children'), 
-          [Input('table1', 'active_cell'),Input('table1', 'data')])
-def update_graphs(value,data):
+@callback(Output('tbl_out1', 'children'),
+          [Input('table1', 'active_cell'), Input('table1', 'data')])
+def update_graphs(value, data):
     if value and data:
         row = value['row']
         col_id = value['column_id']
         iets = data[row][col_id]
-        
+
         caption = iets[:-4] + 'caption.txt'
         if caption in all_captions.keys():
             text = all_captions[caption]
         else:
             text = 'No caption was supplied'
-        fig = px.imshow(np.array(all_pictures[iets]), title  = "Caption:" + text)
+        fig = px.imshow(np.array(all_pictures[iets]), title="Caption:" + text)
     else:
         raise PreventUpdate
-        
-    return  dcc.Graph(figure=fig) if value else "Click the table"
+
+    return dcc.Graph(figure=fig) if value else "Click the table"
+
 
 @app.callback(
-    Output('dd-output-container1', 'children'),Output('table1', 'children'),
+    Output('dd-output-container1', 'children'), Output('table1', 'children'),
     Input('demo-dropdown1', 'value')
 )
 def update_output(value):
     if value:
         df = all_data_frames[value]
-        q = dash_table.DataTable(df.to_dict('records'),[{"name": i, "id": i} for i in df.columns], id='table1')
+        q = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns], id='table1')
     else:
         raise PreventUpdate
-        
+
     return f'You have selected {value}', q
 
+
 ################################### User x ###################################
-@callback(Output('tbl_out2', 'children'), 
-          [Input('table2', 'active_cell'),Input('table2', 'data')])
-def update_graphs2(value,data):
+@callback(Output('tbl_out2', 'children'),
+          [Input('table2', 'active_cell'), Input('table2', 'data')])
+def update_graphs2(value, data):
     if value and data:
         row = value['row']
         col_id = value['column_id']
         iets = data[row][col_id]
-        
+
         caption = iets[:-4] + 'caption.txt'
         if caption in all_captions.keys():
             text = all_captions[caption]
         else:
             text = 'No caption was supplied'
-        fig = px.imshow(np.array(all_pictures[iets]), title  = "Caption:" + text)
+        fig = px.imshow(np.array(all_pictures[iets]), title="Caption:" + text)
     else:
         raise PreventUpdate
-        
-    return  dcc.Graph(figure=fig) if value else "Click the table"
+
+    return dcc.Graph(figure=fig) if value else "Click the table"
+
 
 @app.callback(
-    Output('dd-output-container2', 'children'),Output('table2', 'children'),
+    Output('dd-output-container2', 'children'), Output('table2', 'children'),
     Input('demo-dropdown2', 'value')
 )
 def update_output2(value):
     if value:
         df = all_data_frames[value]
-        q = dash_table.DataTable(df.to_dict('records'),[{"name": i, "id": i} for i in df.columns], id='table2')
+        q = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns], id='table2')
     else:
         raise PreventUpdate
-        
-    return f'You have selected {value}', q
 
+    return f'You have selected {value}', q
 
 
 ################################### Callbacks page 3 ##################################
@@ -380,13 +357,8 @@ def update_output2(value):
 ################################### Callbacks page 4 ##################################
 
 
-
 ############## Runnen van dashboard #####################
 
-    
+
 if __name__ == '__main__':
-
-    app.run_server(debug=True,)
-    
-
-    
+    app.run_server(debug=True, )
